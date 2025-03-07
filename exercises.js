c78557213cd18dbcf892846a01cc4f193b810125
Gustavo Guimaraes Franca <2024533@student.cct.ie>
2025-03-07 13:16:33 +0000

    Exercices 7 and 8

--- exercises.js
+++ exercises.js
@@ -18,3 +18,29 @@ for (let i = 0; i < names.length; i++) {
      return answer;
  }
 console.log(stringReverser("JS is the best!"));
+
+//7. Create a function that checks whether a given word or phrase is a palindrome. A
+// palindrome is a word or phrase that reads the same backwards as forwards
+
+function isPalindrome(str) {
+    let reversed = stringReverser(str);
+    return str === reversed;
+}
+console.log(isPalindrome("arara"));
+
+//8. Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"
+// instead of the number, and for the multiples of 5, print "Buzz." For numbers which are
+// multiples of both 3 and 5, print "FizzBuzz."
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+
+
+    }
